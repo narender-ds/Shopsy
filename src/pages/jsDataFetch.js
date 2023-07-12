d377dad1052e5441adbc39306dbf4441a220ab64
narender-ds <narender.singh@dotsquares.com>
2023-07-12 12:48:51 +0530

    Initial Commited

---
+++ src/pages/jsDataFetch.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function JsDataFetch() {
+  let allData = [];
+  var dataToPass = ["villas", "beach", "resturant"];
+  const amentiesShow = (data) => {
+    return data.map((item) => {
+      async function abc(item) {
+        const response = await fetch(
+          `https://api.mapbox.com/geocoding/v5/mapbox.places/${item}.json?access_token=token`
+        );
+        var data = await response.json();
+
+        allData.push(...data.features);
+        console.log("data", allData);
+      }
+      abc(item);
+    });
+  };
+
+  console.log("allData", allData);
+  amentiesShow(dataToPass);
+  return (
+    <div>
+      {/* <button onClick={() => amentiesShow(dataToPass)}>hitMe</button> */}
+    </div>
+  );
+}
+
+export default JsDataFetch;
